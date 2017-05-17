730cc12e0ffab8d4a66f94f9d7263657d1ea59ac
Mochamad Gufron <mgufronefendi@gmail.com>
2017-05-17 14:52:01 +0700

    modify gluu config default

--- config/gluu-wrapper.php
+++ config/gluu-wrapper.php
@@ -1,38 +1,35 @@
 <?php
-
-$gluuHost = env('GLUU_HOST', 'https://dev.idp.kw.com');
+$idpEndpoint = env('IDP_ENDPOINT', 'https://dev.idp.kw.com');
 
 return [
-    'algorithm' => env('GLUU_ALGORITHM', 'HS256'),
-    'authorization_endpoint' => $gluuHost.'/oxauth/seam/resource/restv1/oxauth/authorize',
-    'token_endpoint' => $gluuHost.'/oxauth/seam/resource/restv1/oxauth/token',
-    'userinfo_endpoint' => $gluuHost.'/oxauth/seam/resource/restv1/oxauth/userinfo',
-    'clientinfo_endpoint' => $gluHost.'/oxauth/seam/resource/restv1/oxauth/clientinfo',
+    'algorithm' => 'HS256',
+    'authorization_endpoint' => "{$idpEndpoint}/oxauth/seam/resource/restv1/oxauth/authorize",
+    'token_endpoint' => "{$idpEndpoint}/oxauth/seam/resource/restv1/oxauth/token",
+    'userinfo_endpoint' => "{$idpEndpoint}/oxauth/seam/resource/restv1/oxauth/userinfo",
+    'clientinfo_endpoint' => "{$idpEndpoint}/oxauth/seam/resource/restv1/oxauth/clientinfo",
 
     // This client_id and client_secret is used as self-consumed keys.
-    'client_id' => env('GLUU_CLIENT_ID', '@!8EF4.0267.10A3.7789!0001!58DE.5ADC!0008!66CE.92B9'),
-    'client_secret' => env('GLUU_CLIENT_SECRET', 'refactory'),
+    'client_id' => env('IDP_CLIENT_ID', '@!8EF4.0267.10A3.7789!0001!58DE.5ADC!0008!66CE.92B9'),
+    'client_secret' => env('IDP_CLIENT_SECRET', 'refactory'),
 
-    'response_type' => env('GLUU_RESPONSE_TYPE', 'code'),
-    'scope' => env('GLUU_SCOPE', 'openid email profile'),
+    'response_type' => 'code',
+    'scope' => 'openid',
 
-    'grant_type' => env('GLUU_GRANT_TYPE', 'authorization_code'),
-    'client_assertion_type' => env('GLUU_CLIENT_ASSERTION_TYPE', 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'),
+    'grant_type' => 'authorization_code',
+    'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
 
-    // Set this value to true to automatically store your token.
-    // Make sure that you have migrate the table.
-    'autosave' => env('GLUU_TOKEN_AUTOSAVE', true),
-    'table_name' => env('GLUU_TOKEN_TABLENAME', 'access_tokens'),
+    // Set table name to store OpenID credentials
+    'table_name' => 'access_tokens',
+
+    // Set table name of user authentication which implements AuthenticatableContract
+    'user_table_name' => 'users',
 
     // Endpoint for user to access
-    'route_endpoint' => env('GLUU_LOGIN_ENDPOINT', '/api/v1/login'),
+    'route_endpoint' => '/login',
 
     // Callback routes
-    'route_access_token_granted' => env('GLUU_TOKEN_GRANTED_ENDPOINT', '/callback'),
-
-    // users table name
-    'user_table_name' => env('GLUU_USER_TABLE', 'users'),
+    'route_access_token_granted' => '/access_granted',
 
     // Route for getting user info
-    'route_get_user_info' => env('GLUU_USER_INFO_ENDPOINT', '/user_info/{access_token}'),
+    'route_get_user_info' => '/user_info/{access_token}',
 ];
